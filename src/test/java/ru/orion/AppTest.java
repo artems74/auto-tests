4dad7e9886df9c93238a6749776ec0ff2b250a84
Artem Sirota <sirota@virtualhealth.com>
2020-11-06 16:47:03 +0500

    Initial commit

---
+++ src/test/java/ru/orion/AppTest.java
@@ -0,0 +1,49 @@
+package ru.orion;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class AppTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstAppTest() {
+        driver.navigate().to("http://www.google.com");
+        driver.findElement(By.className("J9leP")).click();
+        driver.findElement(By.id("K32")).click();
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnK")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+    }
+}
